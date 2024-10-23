6f8cd9ecc9ed8dab7341498b0428f56854d10a96
weiningwei <1479796521@qq.com>
2024-10-23 17:20:23 +0800

    添加冒泡排序和选择排序

--- code/main.cpp
+++ code/main.cpp
@@ -1,8 +1,32 @@
 #include "sort_algorithm.h"
 #include <iostream>
 
+template<typename T>
+void print_arr(T arr[], int length)
+{
+    for (int i = 0; i < length; ++i)
+        std::cout << arr[i] << ' ';
+    std::cout << std::endl;
+}
+
 int main()
 {
-    std::cout << "hello\n";
+    int arr[] = {61, 17, 29, 22, 34, 60, 72, 21, 50, 1, 62};
+    int len = (int)sizeof(arr) / sizeof(*arr);
+
+#if 0
+    sort_bubble(arr, len);
+    print_arr(arr, len);
+
+    sort_bubble(arr, len, false);
+    print_arr(arr, len);
+#endif
+
+    sort_select(arr, len);
+    print_arr(arr, len);
+
+    sort_select(arr, len, false);
+    print_arr(arr, len);
+    
     return 0;
 }
--- include/sort_algorithm.h
+++ include/sort_algorithm.h
@@ -1,5 +1,76 @@
 #ifndef _SORT_ALGORITHM_H
 #define _SORT_ALGORITHM_H
 
+// sort_flag
+// 1 升序 默认
+// 0 降序
+
+template <typename T>
+void sort_swap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+// 冒泡排序
+template <typename T>
+void sort_bubble(T arr[], int length, bool flag = true)
+{
+    for (int i = 0; i < length - 1; ++i)
+    {
+        if (flag)
+        {
+            for (int j = 0; j < length - 1 - i; ++j)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    sort_swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+        else
+        {
+            for (int j = length - 1; j > i; --j)
+            {
+                if (arr[j - 1] < arr[j])
+                {
+                    sort_swap(arr[j - 1], arr[j]);
+                }
+            }
+        }
+    }
+}
+
+// 选择排序
+template <typename T>
+void sort_select(T arr[], int length, bool flag = true)
+{
+    for (int i = 0; i < length - 1; ++i)
+    {
+        int k = i;
+        if (flag)
+        {
+
+            for (int j = i + 1; j < length; ++j)
+            {
+                if (arr[j] < arr[k])
+                {
+                    sort_swap(arr[j], arr[k]);
+                }
+            }
+        }
+        else
+        {
+            for (int j = i + 1; j < length; ++j)
+            {
+                if (arr[j] > arr[k])
+                {
+                    sort_swap(arr[j], arr[k]);
+                }
+            }
+        }
+    }
+}
 
 #endif
